77be12539bd675b3de3ce19d00cf948985f689f3
VenkataPraveen-09 <2000031043cse@gmail.com>
2023-10-05 22:17:52 +0530

    modified:   public/index.html 	modified:   src/App.js 	new file:   src/components/About.js 	new file:   src/components/Home.js 	new file:   src/components/Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>THis is INotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar title="NoteBook" />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>THis is INotebook</h1>
+    This is Home
+    </div>
+  )
+}
+
+export default Home
