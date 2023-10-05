8ebd204be6cbb552b0c89418f03bd8bbe2e64da7
VenkataPraveen-09 <2000031043cse@gmail.com>
2023-10-05 12:29:55 +0530

    modified:   backend/index.js 	modified:   backend/package-lock.json 	modified:   backend/package.json

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+    const express = require ('express')
+    const mongoose =require('mongoose')
+    const app=express()
+    
+    mongoose.connect('mongodb://127.0.0.1:27017/crud')
+    app.use(express.json())
+    // const userSchema=mongoose.Schema({
+    //     Age : Number,
+    //     Name : String
+    // })
+
+    // const UserModel=mongoose.model("users",userSchema)
+
+    // app.get("/getUsers",(req,res)=>{
+    //     UserModel.find({}).then(function(users){
+    //         res.json(users)
+    //     }).catch(function(err){
+    //         console.log(err)
+    //     })
+    // })
+    app.get ('/',(req,res)=> {
+        res.send('Hello World')
+    })
+    // app.get ('/api/v1/login',(req,res)=> {
+    //     res.send('Hello Login')
+    // })
+    // app.get ('/api/v1/signup',(req,res)=> {
+    //     res.send('Hello Signup')
+    // })
+    app.use('/api/auth',require('./routes/auth'))
+    app.use('/api/notes',require('./routes/notes'))
+    app.listen(3001,()=>{
+        console.log("Connected")
+    })
