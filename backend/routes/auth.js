3c7b42e918cf86ac3a1582e049cbd6ebc73b9c88
VenkataPraveen-09 <2000031043cse@gmail.com>
2023-10-08 02:09:42 +0530

    modified:   backend/routes/auth.js 	modified:   src/App.js 	new file:   src/components/Login.js 	modified:   src/components/Navbar.js 	new file:   src/components/Signup.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -73,6 +73,7 @@ router.post(
     body("password", "password cannot be blank").exists(),
   ],
   async (req, res) => {
+    let success=false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -87,9 +88,10 @@ router.post(
       }
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
+        success=false;
         return res
           .status(400)
-          .json({ error: "Please try to login with correct credentials" });
+          .json({ success,error: "Please try to login with correct credentials" });
       }
       const data = {
         user: {
@@ -97,7 +99,8 @@ router.post(
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json(authtoken);
+      success=true;
+      res.json({success,authtoken});
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error");
