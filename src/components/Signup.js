6837156f857a5e74aa0cad9057434016734c9268
VenkataPraveen-09 <2000031043cse@gmail.com>
2023-10-08 23:07:48 +0530

    modified:   public/index.html 	new file:   public/logo.png 	new file:   src/components/Signup.css 	modified:   src/components/Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+// import './Signup.css';
 const Signup = (props) => {
   const[credentials,setCredentials]= useState({name:"",email:"",password:"",cpassword:""})
   let navig=useNavigate();
@@ -31,31 +31,77 @@ const onChange=(e)=>{
     setCredentials({...credentials,[e.target.name]:e.target.value})
 }
 
-  return (
-    <div className='container my-2'>
-      <h3>Create an account to use Inotebook</h3>
+return (
+  <div className="container">
+    <h3>Create an account to use Inotebook</h3>
     <form onSubmit={handleSubmit}>
-    <div className="mb-3">
-    <label htmlFor="name" className="form-label">Name</label>
-    <input type="text" className="form-control" id="name" name="name" onChange={onChange} aria-describedby="emailHelp"/>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          name="name"
+          onChange={onChange}
+          aria-describedby="emailHelp"
+          value={credentials.name}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          name="email"
+          onChange={onChange}
+          aria-describedby="emailHelp"
+          value={credentials.email}
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          onChange={onChange}
+          minLength={5}
+          required
+          value={credentials.password}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="cpassword" className="form-label">
+          Confirm Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="cpassword"
+          name="cpassword"
+          onChange={onChange}
+          minLength={5}
+          required
+          value={credentials.cpassword}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
   </div>
-  <div className="mb-3">
-    <label htmlFor="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="email"name="email" onChange={onChange} aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="password" className="form-label">Password</label>
-    <input type="password" className="form-control" id="password" name="password" onChange={onChange} minLength={5} required/>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-    <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} minLength={5} required/>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-    </div>
-  )
-}
+);
+};
 
-export default Signup
+export default Signup;
